d392b348753e0b5f44c182e6c12ddbf2ae306fc3
PizzaCrust <pizzacrustcoder@gmail.com>
2017-03-19 15:22:34 +0700

    Create api sourcesets

--- src/api/java/capiscum/api/component/Component.java
+++ src/api/java/capiscum/api/component/Component.java
@@ -3,41 +3,41 @@ package capiscum.api.component;
 import java.util.Optional;
 
 /**
- * A component is a identifiable piece of information that can be linked into another object.
+ * A capiscum.api.component is a identifiable piece of information that can be linked into another object.
  *
- * For example, a component can be used to store information; such as a delegation of a
+ * For example, a capiscum.api.component can be used to store information; such as a delegation of a
  * implementation of a API class, without directly interfacing with the class. It can be accessed
- * through a static source, compared and retrieve the class from the data in the component.
+ * through a static source, compared and retrieve the class from the data in the capiscum.api.component.
  *
  * It is mutable and also, is not capable of outputting null values without forcing it.
  *
  * @since 1.0-SNAPSHOT
  * @author PizzaCrust
- * @param <V> the value type of the component
+ * @param <V> the value type of the capiscum.api.component
  */
 public interface Component<V> {
 
     /**
-     * Sets the data of the component.
+     * Sets the data of the capiscum.api.component.
      * @param value
      */
     void set(V value);
 
     /**
-     * Retrieves the stored data of the component.
+     * Retrieves the stored data of the capiscum.api.component.
      * @return
      */
     Optional<V> get();
 
     /**
-     * Determines if the component specified equals another component.
+     * Determines if the capiscum.api.component specified equals another capiscum.api.component.
      * @param component
      * @return
      */
     boolean equals(Component<V> component);
 
     /**
-     * Creates a string component.
+     * Creates a string capiscum.api.component.
      * @param component
      * @return
      */
